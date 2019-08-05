8ca48ac8b0b5e16f22bac82ecda2d687a0a8d22f
Antony <lv999k@hotmail.com>
2019-08-04 23:15:44 -0500

    :rocket: First commit

---
+++ Assets/Scripts/DragHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public static GameObject objBeingDraged;
+
+    private Vector3 startPosition;
+    private Transform startParent;
+    private CanvasGroup canvasGroup;
+    private Transform itemDraggerParent;
+
+    private void Start() 
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        itemDraggerParent = GameObject.FindGameObjectWithTag("ItemDraggerParent").transform;
+    }
+
+    #region DragFunctions
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        objBeingDraged = gameObject;
+
+        startPosition = transform.position;
+        startParent = transform.parent;
+        transform.SetParent(itemDraggerParent);
+
+        canvasGroup.blocksRaycasts = false;
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        objBeingDraged = null;
+
+        canvasGroup.blocksRaycasts = true;
+        if (transform.parent == itemDraggerParent)
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent);
+        }
+    }
+
+    #endregion
+
+    private void Update()
+    {
+
+    }
+}
